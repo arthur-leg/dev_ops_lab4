f19c85900d9315e8f6ce3bb2775cc56c2942ec6d
arthur-leg <129770298+arthur-leg@users.noreply.github.com>
2026-05-19 09:35:58 +0200

    errorfix

--- webhook_listener/deploy.ps1
+++ webhook_listener/deploy.ps1
@@ -24,13 +24,13 @@ Write-Host "Starting app..."
 $npmScripts = (npm run) | Out-String
 # if we built our React app app with create-react-app
 if ($npmScripts -match "\sstart\s") {
- Start-Process -FilePath "npm" -ArgumentList "run","start" -
+ Start-Process -FilePath "npm.cmd" -ArgumentList "run","start" -
 RedirectStandardOutput "app.log" -RedirectStandardError "app.err.log"
 Write-Host "Started with: npm run start"
 }
 # if we built our React app app with vite
 elseif ($npmScripts -match "\sdev\s") {
- Start-Process -FilePath "npm" -ArgumentList "run","dev","--","--
+ Start-Process -FilePath "npm.cmd" -ArgumentList "run","dev","--","--
 host","0.0.0.0" -RedirectStandardOutput "app.log" -RedirectStandardError
 "app.err.log"
  Write-Host "Started with: npm run dev"
